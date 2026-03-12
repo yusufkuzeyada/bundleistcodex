a1cfa06ffd317652b6d00ef3b72d0374a18d7618
Yusuf Bicer <elchicoguapo@gmail.com>
2026-03-12 19:41:33 +0300

    Fix desktop localized hero text

--- site/assets/arrow-left.js
+++ site/assets/arrow-left.js
@@ -1,1 +1,1 @@
-import{c as t}from"./app.js?v=20260312j";const e=t("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);export{e as A};
+import{c as t}from"./app.js?v=20260312k";const e=t("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);export{e as A};
